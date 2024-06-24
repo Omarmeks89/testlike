a4f3b77acfc22cef1911f657b694dc94e5039e3e
Егор Марков <e.markov@gt-sot.ru>
2024-06-24 21:22:22 +0300

    added license preamble into a header

--- ctest.h
+++ ctest.h
@@ -1,3 +1,19 @@
+/* Simple lightweight testing util for C programs.
+* Copyright (C) 2024  Egor Markov
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef _CTEST_H
 #define _CTEST_H
 
