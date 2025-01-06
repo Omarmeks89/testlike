607dd25808ff9d46f455f20ed6e401f18b0ced85
Егор Марков <e.markov@gt-sot.ru>
2025-01-06 21:42:15 +0300

    FEATURE: add test for utf16 surrogate

--- test/strtest.c
+++ test/strtest.c
@@ -16,14 +16,28 @@ void test_2byte_utf8_seq()
     ASSERT_EQ_INT32(i, 2, "check_byte_ptr", LINE());
 }
 
+void test_detect_utf16_surrogate()
+{
+    char example[2] = {0xDF, 0x00};
+    char curr[2] = {0xDF, 0x00};
+    int res = -1, i = 0;
+
+    res = is_utf8_2byte_symbol(example, curr, &i, &eq_bytes);
+    ASSERT_EQ_INT32(res, NOTUTF, "test_utf16_is_notutf8", LINE());
+}
+
 void test_eq_bytes()
 {
     int res = 0;
     char sample[1] = {0x7F};
     char curr[1] = {0x80};
+    char valid_curr[1] = {0x7F};
 
     res = eq_bytes(sample, curr);
     ASSERT_EQ_INT32(res, 1, "test_eq_bytes", LINE());
+
+    res = eq_bytes(sample, valid_curr);
+    ASSERT_EQ_INT32(res, 0, "test_eq_valid_bytes", LINE());
 }
 
 int main()
@@ -31,5 +45,6 @@ int main()
     locale_info_test();
     test_2byte_utf8_seq();
     test_eq_bytes();
+    test_detect_utf16_surrogate();
     return 0;
 }
